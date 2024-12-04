b3cca86d6e6493a0e6456b4eafa56bef08d44c5e
genki <genki@cps.im.dendai.ac.jp>
2024-12-05 03:10:37 +0900

    データ数削減

--- dataset/audio_dataset.py
+++ dataset/audio_dataset.py
@@ -14,10 +14,10 @@ class AudioDatasets(torch.utils.data.Dataset):
     
     def __getitem__(self,idx):
         data,_ = librosa.load(self.path[idx][0],sr=self.sr)
-        if int(data.shape[0]/self.sr) >= 10:
-            data = data[:self.sr*10]
-        elif int(data.shape[0]/self.sr) < 10:
-            data = np.pad(data,pad_width=(0,self.sr*10-data.shape[0]), mode='constant', constant_values=0)
-        data = data.reshape(1,self.sr*10)
+        if int(data.shape[0]/self.sr) >= 5:
+            data = data[:self.sr*5]
+        elif int(data.shape[0]/self.sr) < 5:
+            data = np.pad(data,pad_width=(0,self.sr*5-data.shape[0]), mode='constant', constant_values=0)
+        data = data.reshape(1,self.sr*5)
         label = np.array(self.path[idx][1])
         return data, label
