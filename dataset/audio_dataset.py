990eabd74b009bab3c1e219a6040de191ef97a32
genki <genki@cps.im.dendai.ac.jp>
2024-12-06 15:14:22 +0900

    秒数変更

--- dataset/audio_dataset.py
+++ dataset/audio_dataset.py
@@ -14,9 +14,9 @@ class AudioDatasets(torch.utils.data.Dataset):
     
     def __getitem__(self,idx):
         data,_ = librosa.load(self.path[idx][0],sr=self.sr)
-        if int(data.shape[0]/self.sr) >= 1:
+        if int(data.shape[0]/self.sr) >= 3:
             data = data[:self.sr*1]
-        elif int(data.shape[0]/self.sr) < 1:
+        elif int(data.shape[0]/self.sr) < 3:
             data = np.pad(data,pad_width=(0,self.sr*1-data.shape[0]), mode='constant', constant_values=0)
         data = data.reshape(1,self.sr*1)
         label = np.array(self.path[idx][1])
--- model/model.py
+++ model/model.py
@@ -22,10 +22,11 @@ class Net1D(nn.Module):
                                    nn.MaxPool1d(kernel_size=7, stride=2),
                                   )
         
-        self.conv4 = nn.Sequential(nn.Conv1d(64,128,kernel_size=9,stride=1),
+        self.conv4 = nn.Sequential(nn.Conv1d(64, 128, kernel_size=9, stride=1),
                                    nn.BatchNorm1d(128),
                                    nn.ReLU(inplace=True),
-                                   nn.MaxPool1d(kernel_size=9,stride=2),)
+                                   nn.MaxPool1d(kernel_size=9, stride=2),
+                                  )
                                    
  
         self.dense = nn.Sequential(nn.Linear(128*2988, 512),
